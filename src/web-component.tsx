d391510e7f83d5ade11e5c9cf7eae2ea6771be15
sauvik <sauvik@logic-square.com>
2025-04-01 18:06:09 +0530

    css styles not loading fix

--- src/web-component.tsx
+++ src/web-component.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { WidgetContainer } from "./widget/components/widget-container";
 import { IChatWidgetProps } from "./widget/components/widget-container";
-import "./widget/styles/style.css";
+import styles from "./widget/styles/style.css?inline"; // Load CSS as a string
 
 const normalizeAttribute = (attribute: string) => {
   return attribute?.replace(/-([a-z])/g, (_, letter) => letter?.toUpperCase());
@@ -15,6 +15,13 @@ class ChatWidgetWebComponent extends HTMLElement {
 
   connectedCallback() {
     const props = this.getPropsFromAttributes<IChatWidgetProps>();
+
+    // Inject styles inside Shadow DOM
+    const styleTag = document.createElement("style");
+    styleTag.textContent = styles;
+    this.shadowRoot?.appendChild(styleTag); // Append CSS
+
+    // Render React component
     const root = ReactDOM.createRoot(this.shadowRoot as ShadowRoot);
     root.render(<WidgetContainer {...props} />);
   }
